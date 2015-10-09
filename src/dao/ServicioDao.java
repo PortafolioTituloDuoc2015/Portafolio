74ea562819b90cd9cd806dbcadc5cf36b0261662
Yoyin <Yoyin@Yoyo>
2015-10-09 09:12:48 -0300

    Actualizacion a portafolio Master

--- src/dao/ServicioDao.java
+++ src/dao/ServicioDao.java
@@ -5,10 +5,12 @@
  */
 package dao;
 import dao.*;
+import static dao.UsuarioDao.conn;
 import java.sql.*;
 import javax.swing.*;
 import sql.Conexion;
 import dto.*;
+import java.util.ArrayList;
 /**
  *
  * @author Yoyin
@@ -66,5 +68,70 @@ public class ServicioDao {
         return existe;
     }
     
+    public static void modificarServicio(ServicioDto dto){
+        try{
+            Connection conexion= Conexion.Enlace(conn);
+            String query=("UPDATE servicio SET nombre=?, descripcion=?,"
+                    + "precio=?");
+            PreparedStatement modificar = conexion.prepareStatement(query);
+                modificar.setString(1,dto.getNombreServicio());
+                modificar.setString(2, dto.getDescripcion());
+                modificar.setInt(3, dto.getPrecio());
+                modificar.execute();
+                modificar.close();
+                conexion.close();
+            }catch(SQLException z){
+            System.out.println("Error SQL al modificarServicio: "+z.getMessage());
+            System.out.println("Error SQL al modificarServicio: "+z.getCause());
+            }catch(Exception e){
+                System.out.println("Error al modificarServicio:"+e.getMessage());
+                System.out.println("Error al modificarServicio:"+e.getCause());
+            }
+    }
+    
+    public static ArrayList<ServicioDto> listarTodos(){
+        ArrayList<ServicioDto> lista = new ArrayList<ServicioDto>();
+        try{
+                Connection conexion = Conexion.Enlace(conn);
+                String query = "SELECT * FROM servicio";
+                PreparedStatement listar = conexion.prepareStatement(query);
+                ResultSet rs = listar.executeQuery();
+                while (rs.next()) {
+                    ServicioDto dto = new ServicioDto();
+                    dto.setIdServicio(rs.getInt("idServicio"));
+                    dto.setNombreServicio(rs.getString("nombreServicio"));
+                    dto.setDescripcion(rs.getString("descripcion"));
+                    dto.setPrecio(rs.getInt("precio"));
+                    lista.add(dto);
+}
+                listar.close();
+                conexion.close();
+        }catch(SQLException s){
+                System.out.println("Error SQL al listarTodos: "+s.getMessage());
+        }catch(Exception e){
+                System.out.println("Error al listarTodos:"+e.getMessage());
+        }
+        return lista;
+    }
+    
+    
+    public static boolean eliminarServicio(String servicio){
+        boolean elimino = false;
+        try{
+                Connection conexion = Conexion.Enlace(conn);
+                String query = "DELETE FROM servicio WHERE idServicio=?";
+                PreparedStatement eliminar = conexion.prepareStatement(query);
+                eliminar.setString(1, servicio);
+                eliminar.execute();
+                eliminar.close();
+                conexion.close();
+                elimino = true;
+            }catch(SQLException z){
+            System.out.println("Error SQL al eliminarTrabajador: "+z.getMessage());
+            }catch(Exception e){
+                System.out.println("Error al eliminarTrabajador:"+e.getMessage());
+            }
+        return elimino;
+    }
     
 }
